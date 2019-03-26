753cd9f72a40a50c81e1d17f60d6dd2cf49760ca
Tran Thach <tranthachkhtn@gmail.com>
2019-03-26 23:52:34 +0700

    Add Resource

---
+++ 1512520_LuanVanTotNghiep/Assets/Scripts/Common/LineToolTip.cs
@@ -0,0 +1,15 @@
+﻿using HoloToolkit.Unity.UX;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineToolTip : MonoBehaviour {
+    public LineBase lineBase;
+	// Update is called once per frame
+	void Update () {
+        if (transform.GetComponentInChildren<MeshRenderer>().isVisible)
+            lineBase.LineEndClamp = 1;
+        else
+            lineBase.LineEndClamp = 0;
+    }
+}
