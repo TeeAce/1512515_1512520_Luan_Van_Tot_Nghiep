4196ec18462c0d8a10fc6bd12134f4a06f050ca4
1512520_Tran_Van_Thach <tranthachkhtn@gmail.com>
2019-04-01 21:40:21 +0700

    Update SmartPhone Manual

--- 1512520_LuanVanTotNghiep/Assets/Scripts/Common/LineToolTip.cs
+++ 1512520_LuanVanTotNghiep/Assets/Scripts/Common/LineToolTip.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class LineToolTip : MonoBehaviour {
     public LineBase lineBase;
+    public MeshRenderer label;
 	// Update is called once per frame
 	void Update () {
-        if (transform.GetComponentInChildren<MeshRenderer>().isVisible)
+        if (label.isVisible)
             lineBase.LineEndClamp = 1;
         else
             lineBase.LineEndClamp = 0;
