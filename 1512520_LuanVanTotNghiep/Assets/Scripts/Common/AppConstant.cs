0bf1dfbfacda25c6673cfc80a5b99dd0fe8eb142
1512520_Tran_Van_Thach <tranthachkhtn@gmail.com>
2019-05-28 13:16:06 +0700

    Add socket io

--- 1512520_LuanVanTotNghiep/Assets/Scripts/Common/AppConstant.cs
+++ 1512520_LuanVanTotNghiep/Assets/Scripts/Common/AppConstant.cs
@@ -7,3 +7,17 @@ public class AppConstant{
     public static string PATH_CAMERA_TEXTURE_OUT_PUT = Application.dataPath+ "/Resources/Output/";
     public static string PATH_RECORGNIZE_IN_PUT = Application.dataPath + "/Resources/Input/";
 }
+
+public class MS_CLIENT_TO_SERVER
+{
+    public static string CHECK_CONNECTED        = "CHECK_CONNECTED";
+    public static string CLIENT_TO_SERVER       = "CLIENT_TO_SERVER";
+    public static string REQUEST_FEATURE        = "REQUEST_FEATURE";
+}
+
+public class MS_SERVER_TO_CLIENT
+{
+    public static string USER_CONNECTED         = "USER_CONNECTED";
+    public static string RESPONSE_FEATURE       = "RESPONSE_FEATURE";
+
+}
---
+++ 1512520_LuanVanTotNghiep/Assets/Scripts/Controller/Client.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using System;
+
+public class Client : MonoBehaviour {
+
+    public SocketIOComponent socket;
+    JSONObject jSONObject = new JSONObject();
+
+    public void Start()
+    {
+        AddListener();
+        StartCoroutine(CheckConnectToServer());
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void AddListener()
+    {
+        socket.On(MS_SERVER_TO_CLIENT.USER_CONNECTED, OnUserConnected);
+        socket.On(MS_SERVER_TO_CLIENT.RESPONSE_FEATURE, OnResponseFeature);
+    }
+
+    private void OnResponseFeature(SocketIOEvent obj)
+    {
+
+        string data = obj.data.ToString();
+        Debug.Log(data);
+        Features f = JsonUtility.FromJson<Features>(data);
+
+        Debug.Log(f.features[0].ToString());
+    }
+
+    IEnumerator CheckConnectToServer()
+    {
+        yield return new WaitForSeconds(3);
+        socket.Emit(MS_CLIENT_TO_SERVER.CHECK_CONNECTED);
+    }
+
+    public void OnUserConnected(SocketIOEvent evt)
+    {
+        Debug.Log("User Connected");
+        RequestFeature();
+    }
+
+    public void RequestFeature()
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["id"] = "2";
+        socket.Emit(MS_CLIENT_TO_SERVER.REQUEST_FEATURE, new JSONObject(data));
+    }
+}
+
+public class Features
+{
+    public string[] features;
+}
